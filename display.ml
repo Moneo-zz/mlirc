19af0918042eb69497fe6a973e2647f2614cf481
Pierre Chopin <pierre@punchup.com>
2012-02-28 14:34:46 -0500

    enhanced connection

--- display.ml
+++ display.ml
@@ -1,9 +1,13 @@
 open Graphics
+
 let row_height = 15
 let offset=20
+
+
 type row = {content:string;
 	    mutable rank:int;
 	   }
+
 
 let clear_row r = 
  set_color white;
@@ -16,14 +20,24 @@ let print_row r =
   moveto offset (row_height*r.rank);
   draw_string r.content
 
-let incr_row r  =
-   r.rank <- (r.rank + 1)
 
 
+let rows=ref []
 
-let rows = ref []
+let number_of_rows = 60
+
+let rec incr_rows res = function
+	[] -> res
+	|a::b when a.rank < number_of_rows-> 
+			begin
+				a.rank <- a.rank+1;
+				 incr_rows (a::res) b
+			end
+	|a::b -> incr_rows res b
+
+
 let add_row s= 
-	List.iter incr_row !rows;
-	rows:={content=s;rank=1}::!rows;
+	rows:={content=s;rank=1}::(incr_rows [] !rows);
 	List.iter print_row !rows
-
+let init () = 
+  Graphics.open_graph ":0 500x700"
--- mlirc.ml
+++ mlirc.ml
@@ -1,42 +1,118 @@
-open Unix 
+(*==================================================*)
+(*             Parsing user input                   *)
 
-let server = Sys.argv.(1)
-let nick=Sys.argv.(2)
-let addr = (gethostbyname server).h_addr_list.(0)
-let s = socket PF_INET SOCK_STREAM 0 
-let _ = clear_nonblock s;
-connect s  (ADDR_INET (addr,6667))
-let cin = in_channel_of_descr s
-let cout = out_channel_of_descr s
+type user_input = (*two types of inputs *)
+	Command of string * string
+	|Message of string
 
+(*regexps for parsing command *)
+
+let command = Str.regexp "/[^ ]*"
+let space = Str.regexp " "
+
+let strip_leading s = (*removes the first char*)
+	String.sub s 1 (String.length s -1) 
+
+let parse_user s = 
+	let l = Str.split space s in 
+		try 
+			let a::b =  l in
+				if Str.string_match command a 0 then 
+					Command (strip_leading a,String.concat " " b)
+				else
+					Message s
+  with _ -> Message s
+  
+(*==================================================*)
+(*              Parsing server input                *)
+
+type server_input =
+	Ping of string 
+	|Other of string
+
+let cr = Str.regexp "\r"
+
+let parse_server s = 
+	let s = Str.global_replace cr "" s in
+	let l =  Str.split space s in
+		try
+			let a::b = l in 
+				if a = "PING" then 
+					Ping (String.concat " " b) 
+				else 
+					Other s
+		with _ -> Other s
+
+(*==================================================*)
+(*              Connecting to server                *)
+
+let connect () = 
+	let open Unix in 
+		let server = Sys.argv.(1) in
+		let port=int_of_string Sys.argv.(2) in 
+		let addr = (gethostbyname server).h_addr_list.(0) in
+		open_connection (ADDR_INET (addr,port))
+
+
+(*==================================================*)
+(*          Thread safe output functions            *)
 
 open Printf
 module M = Mutex
 
 let m = M.create ()
+(* thread safe writing on the connection socket *)
 
-let print_command s =
+let print_command oc command param =
   M.lock m;
-  fprintf cout "%s\r\n" s;
+  fprintf oc "%s %s \r\n" command param  ;
+  flush oc;
   M.unlock m
 
+let print_message oc chan message = 
+match chan with
+	Some chan ->   
+		M.lock m;
+		fprintf oc "PRIVMSG %s :%s \r\n" chan message;
+		flush oc;
+		M.unlock m
+	|None -> print_endline "You need to join a channel first !"
+(*==================================================*)
+(*                  Main loops                      *)
 
 
-let rec read_stuff ()=
-    let open Str in 
-         let s = (input_line cin) in
-	  let s = String.sub s 0 (String.length s -1) in
-	 let a::b = split (regexp " ") s in 
-         if a="PING" then (
-           print_command (String.concat " " ("PONG"::b));
-	   print_endline "PONG" );
-         Display.add_row s;
-         read_stuff ()
-let _ = Graphics.open_graph ":0 600x700"
+let rec read_stuff ic oc = (*read stuff from the socket *)
+	let open Str in 
+		 match parse_server (input_line ic) with 
+			Ping s -> 
+				print_command oc "PONG" s;
+				read_stuff ic oc
+			|Other s -> Display.add_row s;
+				read_stuff ic oc
+
+let init oc = 
+	print_endline "please enter your nickname:" ;
+	let nick = read_line () in 
+		print_command oc "USER" "ocaml * * :school" ;
+		print_command oc "NICK" nick
 	
-let _ = print_endline "Client: connected" 
-let _ = Thread.create read_stuff () 
-let _ = while true do 
-   let s = read_line () in 
-   print_command s
-  done
+let rec print_stuff oc chan=
+	match parse_user (read_line () ) with 
+		Command (c,p) when c = "JOIN"-> 
+			print_command oc c p ;
+			print_stuff oc (Some p)
+		|Command (c,p) -> 	
+			print_command oc c p;
+			print_stuff oc chan
+		|Message m -> 
+			print_message oc chan m ;
+			 Display.add_row (sprintf "You sent: %s" m);
+			print_stuff oc chan
+
+let _ = 
+	let ic,oc = connect () in 
+		Display.init ();
+		Thread.create (read_stuff ic) oc;
+		init oc;
+		print_endline "Client: connected" ; 
+		print_stuff oc None
